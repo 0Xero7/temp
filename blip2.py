4f93efe4a70e30c25a4794e0a00634ef033912c2
Soumya Pattanayak <smpsmp13@gmail.com>
2024-01-08 00:11:45 +0530

    Update blip2.py

--- blip2.py
+++ blip2.py
@@ -4,7 +4,7 @@ from PIL import Image
 from transformers import Blip2Processor, Blip2ForConditionalGeneration
 
 processor = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
-model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", torch.dtype=torch.float16).to("cuda")
+model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", torch_dtype=torch.float16).to("cuda")
 
 img_url = input('Image URL : ') 
 raw_image = Image.open(requests.get(img_url, stream=True).raw)
