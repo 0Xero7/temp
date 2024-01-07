11ce089332a979b8fe2ee8ded559318b4acbfadd
Soumya Pattanayak <smpsmp13@gmail.com>
2024-01-08 00:07:00 +0530

    Update blip2.py

--- blip2.py
+++ blip2.py
@@ -7,10 +7,13 @@ processor = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
 model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b". torch.dtype=torch.float16).to("cuda")
 
 img_url = input('Image URL : ') 
-raw_image = Image.open('./image.jpeg')
+raw_image = Image.open(requests.get(img_url, stream=True).raw)
+
+print('Image loaded')
 
 question = "describe the main keywords you can gather from this image"
 inputs = processor(raw_image, question, return_tensors="pt").to("cuda", torch.float16)
+print('Inputs loaded')
 
 out = model.generate(**inputs)
 print(processor.decode(out[0], skip_special_tokens=True).strip())
