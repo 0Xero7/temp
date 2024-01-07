c6871c87eea38c98f4eedb0060005f11c1d9429c
Soumya Pattanayak <smpsmp13@gmail.com>
2024-01-08 00:31:52 +0530

    Update blip2.py

--- blip2.py
+++ blip2.py
@@ -12,8 +12,9 @@ raw_image = Image.open(requests.get(img_url, stream=True).raw)
 
 print('Image loaded')
 
-question = "describe the main keywords you can gather from this image"
-inputs = processor(raw_image, question, return_tensors="pt").to("cuda", torch.float16)
+question = "What is a detailed description of this image?"
+qtext = f"Question: {question} Answer:"
+inputs = processor(raw_image, qtext, return_tensors="pt").to("cuda", torch.float16)
 print('Inputs loaded')
 
 out = model.generate(**inputs)
