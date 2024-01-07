0688b85f17bbfa9767ebb5db57df23ebf7743827
Soumya Pattanayak <smpsmp13@gmail.com>
2024-01-07 23:58:01 +0530

    Update blip2.py

--- blip2.py
+++ blip2.py
@@ -4,12 +4,12 @@ from PIL import Image
 from transformers import Blip2Processor, Blip2ForConditionalGeneration
 
 processor = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
-model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", device_map="auto")
+model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b").to("cuda")
 
 img_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg' 
 raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
 
-question = "how many dogs are in the picture?"
+question = "describe the main keywords you can gather from this image"
 inputs = processor(raw_image, question, return_tensors="pt").to("cuda")
 
 out = model.generate(**inputs)
