a3a707e5724feaf6d9a4f2257279eddb63daa55e
Soumya Pattanayak <smpsmp13@gmail.com>
2024-01-08 00:35:13 +0530

    Update blip2.py

--- blip2.py
+++ blip2.py
@@ -5,7 +5,7 @@ from PIL import Image
 from transformers import Blip2Processor, Blip2ForConditionalGeneration
 
 processor = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
-model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", torch_dtype=torch.float16).to("cuda")
+model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b").to("cuda")
 
 img_url = input('Image URL : ') 
 raw_image = Image.open(requests.get(img_url, stream=True).raw)
@@ -14,7 +14,7 @@ print('Image loaded')
 
 question = "What would be the main keywords you would use to describe this image?"
 qtext = f"Question: {question} Answer:"
-inputs = processor(raw_image, qtext, return_tensors="pt").to("cuda", torch.float16)
+inputs = processor(raw_image, qtext, return_tensors="pt").to("cuda")
 print('Inputs loaded')
 
 out = model.generate(**inputs)
